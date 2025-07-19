7cd4d26cd826b8c652abdef8f7a7f6d0c0a81bb5
jeifher fuentes <fuentesjeifer@gmail.com>
2025-07-19 15:36:19 -0500

    create login

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,28 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { provideRouter } from '@angular/router';
+// import { importProvidersFrom } from '@angular/core';
+// import { provideAnimations } from '@angular/platform-browser/animations';
+// import { AppComponent } from './app/app.component';
+// import { routes } from './app/app.routes';
+// import { HttpClientModule } from '@angular/common/http';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// bootstrapApplication(AppComponent, {
+//   providers: [
+//     // Para que funcione <router-outlet> y this.router.navigate()
+//     provideRouter(routes),
+
+//     // Para HttpClient en tus servicios (AuthService)
+//     importProvidersFrom(HttpClientModule),
+
+//     // Para Angular Material animations
+//     provideAnimations(),
+//     importProvidersFrom(BrowserAnimationsModule)
+//   ]
+// }).catch(err => console.error(err));
