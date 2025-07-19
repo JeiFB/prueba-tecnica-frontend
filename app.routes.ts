4b1497544e35de6037d34eeb37fe00bbfacd6e50
Jeifher Isaac Fuentes Benavides <107227760+JeiFB@users.noreply.github.com>
2025-07-19 15:34:33 -0500

    Delete app.routes.ts

--- app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  { path: 'login', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  // otras rutas...
-];
